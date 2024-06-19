fcd0ab4f5b7113a17e318c380b7ed7a42b7ed48f
Xande <168040538+cxande@users.noreply.github.com>
2024-06-19 19:48:51 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,12 +1,13 @@
 def int_to_roman (num):
 
+
     val = [1000, 900, 500, 400, 100, 90 ,50 ,40 , 10 ,9 ,5, 4, 1]
     rom = ["M", "CM", "VD", "CD", "C" , "XC" ,"L" ,"XL", "X", "IX" , "V" , "IV" ,"I"]    
 
-
+   
     romano = ''
     i = 0
-
+    
 
     while  num > 0:
 
@@ -16,3 +17,10 @@ def int_to_roman (num):
         i += 1  
 
     return romano
+
+
+
+
+
+
+
